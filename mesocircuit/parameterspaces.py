86b2f4c112dc6cd6a648c80aec9bae5e87c1d8eb
Johanna Senk <j.senk@fz-juelich.de>
2020-07-08 11:29:42 +0200

    Add parameterspaces.py to define parameter spaces

---
+++ mesocircuit/parameterspaces.py
@@ -0,0 +1,19 @@
+import os
+import parameters as ps
+
+ps_dicts = {
+
+    'local_downscale': {
+        'sim_dict': {
+            'computer': 'local',
+            'print_time': True,
+            'data_path': os.path.join(os.getcwd(), 'data_local_downscale')},
+        'net_dict': {
+            'N_scaling': 0.1,
+            'K_scaling': 0.1},
+    }
+}
+
+
+
+# TODO sim_dict for JURECA 
