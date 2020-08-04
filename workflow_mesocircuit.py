7f69bee78cbb12575f7ca3883e34bc97e69b5e68
Johanna Senk <j.senk@fz-juelich.de>
2020-08-04 13:55:57 +0200

    Update workflow

--- workflow_mesocircuit.py
+++ workflow_mesocircuit.py
@@ -2,29 +2,30 @@ import os
 import numpy as np
 import parameterization.helpers as helpers
 
-import analysis.prelim_analysis as ana
+key = 'local_microcircuit'
+#key = 'local_mesocircuit'
 
 parameterview = helpers.evaluate_parameterspaces(filename='parameterspaces',
-paramspace_keys = ['local_microcircuit'])
+paramspace_keys = [key])
 
 # local test run
-data_path, ps_id = parameterview['local_microcircuit'][0]
-#data_path, ps_id = parameterview['local_mesocircuit'][0]
-if 0:
+data_path, ps_id = parameterview[key][0]
+if 1:
     jobscript = os.path.join(data_path, 'jobscripts', ps_id , 'network.sh')
     os.system('sh ' + jobscript)
 
 # TODO submission of jobscripts to JURECA
 
 
-# preliminary analysis just for testing
+# preliminary analysis from microcircuit just for testing
 if 0:
+    import analysis.prelim_analysis as ana
     raster_plot_interval = np.array([500., 700.])
     firing_rates_interval = np.array([500., 1500.])
     param_path = os.path.join(data_path, 'parameters', ps_id)
     ana.evaluate(param_path, raster_plot_interval, firing_rates_interval)
 
-if 0:
+if 1:
     jobscript = os.path.join(data_path, 'jobscripts', ps_id, 'analysis.sh')
     os.system('sh ' + jobscript)
 
