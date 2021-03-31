4e2e9780c70fe5b250519746c2dd99f3a18b0c73
Johanna Senk <j.senk@fz-juelich.de>
2021-03-31 19:05:06 +0200

    Minor

--- mesocircuit/example_theory.py
+++ mesocircuit/example_theory.py
@@ -3,8 +3,6 @@ import core.helpers.mesocircuit_framework as mesoframe
 parameterview = mesoframe.evaluate_parameterspaces(
     with_base_params=True)
 
-# print(parameterview)
-
 # theory is not fully integrated because of dependence on lif_meanfield_tools
 mesoframe.run_parametersets(
     func=mesoframe.run_single_lmt,
