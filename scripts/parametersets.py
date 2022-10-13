8a41ce79172b803b38f015e2c8c9fcf8378ab974
Johanna Senk <j.senk@fz-juelich.de>
2022-10-13 16:29:44 +0100

    Revise parametersets.py

--- scripts/parametersets.py
+++ scripts/parametersets.py
@@ -4,25 +4,7 @@ local_sim_dict = {
     'print_time': True}
 
 ### model-specific definitions ###
-net_dict_microcircuit = {
-    'base_model': 'PD2014',
-    'delay_type': 'normal',
-    'connect_method': 'fixedtotalnumber',
-    'extent': 1.}
-
-net_dict_macaqueV1 = {
-    'base_model': 'SvA2018',
-    'g': -11.,
-    'delay_type': 'normal',
-    'connect_method': 'fixedtotalnumber',
-    'extent': 1.}
-
-net_dict_mesomacaqueV1 = {
-    'base_model': 'SvA2018',
-    'g': -11.
-}
-
-net_dict_favorite = {
+net_dict_mesocircuit_MAMV1 = {
     'base_model': 'SvA2018',
     'g': -11.,
     'neuron_params': {'tau_syn_ex': 2., 'tau_syn_in': 8.},
@@ -30,77 +12,84 @@ net_dict_favorite = {
     'ext_indegree_scaling': np.array([[0, 1.15], [2, 1.1]]),
 }
 
+net_dict_microcircuit_MAMV1 = {
+    'base_model': 'SvA2018',
+    'g': -11.,
+    'delay_type': 'normal',
+    'connect_method': 'fixedtotalnumber',
+    'extent': 1.}
+
+net_dict_microcircuit_PD = {
+    'base_model': 'PD2014',
+    'delay_type': 'normal',
+    'connect_method': 'fixedtotalnumber',
+    'extent': 1.}
+
 ##########################################################################
 
-# main dictionary as used by evaluate_parameterspaces() in helpers.py
 ps_dicts = {
-    'base': {},
 
-    'favorite': {
+    # main mesocircuit based on MAM V1 of Schmidt & van Albada (2018)
+    'mesocircuit_MAMV1': {
         'net_dict': {
-            **net_dict_favorite,
+            **net_dict_mesocircuit_MAMV1,
         },
         'sys_dict': {'hpc': {'network': {'num_nodes': 16}}}
     },
 
-    'favorite_evoked': {
+    # main mesocircuit with spatially confined stimulus
+    'mesocircuit_MAMV1_evoked': {
         'net_dict': {
-            **net_dict_favorite,
+            **net_dict_mesocircuit_MAMV1,
             'thalamic_input': 'pulses',
         },
         'sys_dict': {'hpc': {'network': {'num_nodes': 16}}}
     },
 
-    'microcircuit': {
+    # Potjans & Diesmann (2014) microcircuit
+    'microcircuit_PD': {
         'net_dict': {
-            **net_dict_microcircuit,
+            **net_dict_microcircuit_PD,
         },
         'sys_dict': {'hpc': {'network': {'num_nodes': 1}}}
     },
 
-    'local_microcircuit': {
+    # local mesocircuit
+    'local_mesocircuit_MAMV1': {
+        'sim_dict': {
+            **local_sim_dict,
+        },
         'net_dict': {
-            **net_dict_microcircuit,
+            **net_dict_mesocircuit_MAMV1,
+            'N_scaling': 0.005,
+            'K_scaling': 0.5,
+        },
+    },
+
+    # MAM V1 microcircuit
+    'microcircuit_MAMV1': {
+        'net_dict': {
+            **net_dict_microcircuit_MAMV1,
+        },
+    },
+
+    # local Potjans & Diesmann (2014) microcircuit
+    'local_microcircuit_PD': {
+        'sim_dict': {
+            **local_sim_dict,
+        },
+        'net_dict': {
+            **net_dict_microcircuit_PD,
             'N_scaling': 0.1,
             'K_scaling': 0.1,
         },
     },
 
-    'macaqueV1': {
-        'net_dict': {
-            **net_dict_macaqueV1,
-        },
-    },
+    # mesocircuit based on Potjans & Diesmann (2014)
+    'mesocircuit_PD': {},  # base parameters
 
-    'local_macaqueV1': {
-        'net_dict': {
-            **net_dict_macaqueV1,
-            'N_scaling': 0.1,
-            'K_scaling': 0.1,
-        },
-    },
-
-    'mesomacaqueV1': {
-        'net_dict': {
-            **net_dict_mesomacaqueV1,
-        },
-    },
-
-    'local_mesomacaqueV1': {
-        'sim_dict': {
-            **local_sim_dict,
-        },
-        'net_dict': {
-            **net_dict_mesomacaqueV1,
-            'N_scaling': 0.008,
-            'K_scaling': 0.1,
-        },
-    },
-
-    'local_mesocircuit': {
-        'sim_dict': {
-            **local_sim_dict,
-        },
+    # local mesocircuit based on Potjans & Diesmann (2014)
+    'local_mesocircuit_PD': {
         'net_dict': {
             'N_scaling': 0.008,
             'K_scaling': 0.1,
