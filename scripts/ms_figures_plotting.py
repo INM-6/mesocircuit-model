000dcf7be07bf9fe4b88a161c392d74c8b5b9943
Johanna Senk <j.senk@fz-juelich.de>
2022-11-09 22:34:03 +0000

    Start scripts for manuscript figures

---
+++ scripts/ms_figures_plotting.py
@@ -0,0 +1,7 @@
+"""Plot  manuscript figures
+---------------------------
+
+This script needs to be executed after `ms_figures_simulations.py`.
+"""
+
+################################################################################
