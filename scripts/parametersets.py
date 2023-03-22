fa898954514f1a59e438dd3a2b45ebf91198def7
Johanna Senk (JURECA) <j.senk@fz-juelich.de>
2023-03-22 17:32:27 +0100

    uReplace custom parameters for mesocircuit_MAMV1

--- scripts/parametersets.py
+++ scripts/parametersets.py
@@ -9,8 +9,8 @@ net_dict_mesocircuit_MAMV1 = {
     'base_model': 'SvA2018',
     'g': -11.,
     'neuron_params': {'tau_syn_ex': 2., 'tau_syn_in': 8.},
-    'indegree_scaling': np.array([[5, 4, 0.75], [3, 3, 1.8], [7, 7, 0.9]]),
-    'ext_indegree_scaling': np.array([[0, 1.15], [2, 1.05]]),
+    'indegree_scaling': np.array([[5,4,0.9], [3, 3, 1.8], [7, 7, 0.9], [6,7,0.75], [7, 6, 0.75]]),
+    'ext_indegree_scaling': np.array([[0, 1.1], [2, 1.08]]),
     'ext_indegree_scaling_global': 1.04,
 }
 
