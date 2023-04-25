b050473f0f6483ed2dd687de27ee0893e8c83888
Johanna Senk <j.senk@fz-juelich.de>
2023-04-25 21:54:35 +0200

    Add comments

--- scripts/run_mean_field_theory.py
+++ scripts/run_mean_field_theory.py
@@ -1,5 +1,9 @@
-"""PyNEST Mesocircuit: Run Theory
----------------------------------
+"""PyNEST Mesocircuit: Run Mean-field Theory
+--------------------------------------------
+
+This example script shows a mean-field theoretical analysis using for network
+parameters using
+NNMT - the Neuronal Network Meanfield Toolbox (https://github.com/INM-6/nnmt).
 """
 
 import os
@@ -10,21 +14,33 @@ import nnmt
 import mesocircuit.plotting.plotting as plotting
 import mesocircuit.plotting.figures as figures
 
+################################################################################
+# We instantiate a MesocircuitExperiment with one circuit and use its parameters
+# for creating an NNMT network object.
+
 name = 'microcircuit_MAMV1'  # reference model
-# name = 'microcircuit_PD' # Potjans & Diesmann (2014) microcircuit
-# name = 'mesocircuit_MAMV1' # upscaled model, does not work well
+# name = 'microcircuit_PD'  # Potjans & Diesmann (2014) microcircuit
+
+# upscaled model does not work well because theory does not account for space
+# name = 'mesocircuit_MAMV1'
 custom_params = parametersets.ps_dicts[name]
 meso_exp = mesoframe.MesocircuitExperiment(name, custom_params)
 circuit = meso_exp.circuits[0]
 
-# nnmt network object of type Microcircuit
+# NNMT network object of type Microcircuit
 nw = nnmt.models.Microcircuit(
     network_params=os.path.join(
         circuit.data_dir_circuit, 'parameters', 'nnmt_dict.yaml'),
     analysis_params='nnmt_analysis_params.yaml')
 
+################################################################################
+# We calculate the working point, the transfer function, power spectra, and
+# the sensitivity measure.
+
 # working point for exponentially shaped post synaptic currents
 wp = nnmt.lif.exp.working_point(nw)
+print(wp)
+
 # transfer function
 nnmt.lif.exp.transfer_function(nw)
 # delay distribution matrix
@@ -38,7 +54,9 @@ freqs = nw.analysis_params['omegas'] / (2. * np.pi)
 # sensitivity measure
 sensitivity_dict = nnmt.lif.exp.sensitivity_measure_all_eigenmodes(nw)
 
-# overview figure
+################################################################################
+# The results are plotted as an overview figure.
+
 pl = plotting.Plotting(circuit)
 figures.theory_overview(
     plot=pl,
