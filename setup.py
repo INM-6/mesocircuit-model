ba744e727e793c6b495c98fa03633a66e0de2652
Johanna Senk <j.senk@fz-juelich.de>
2024-09-19 17:15:41 +0100

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mesocircuit',
-      version='1.0',
+      version='1.0.0',
       description='Mesocircuit Model',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
